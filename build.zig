7d1c014e5f67f580e1282a067fba50848fc6bc78
Jack Parsons <jparsons@ualberta.ca>
2025-12-27 16:04:08 -0700

    add a build step for lsp

--- build.zig
+++ build.zig
@@ -19,4 +19,20 @@ pub fn build(b: *std.Build) void {
     });
     runtime_mod.linkLibrary(glfw_dep.artifact("glfw"));
     runtime_mod.linkLibrary(glad_dep.artifact("glad"));
+
+    // Add a check step to populate LSP data
+    const check = b.step("check", "Check if the library compiles");
+    const lib_check = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lib_check.linkLibrary(glfw_dep.artifact("glfw"));
+    lib_check.linkLibrary(glad_dep.artifact("glad"));
+
+    const check_compile = b.addObject(.{
+        .name = "zephyr_runtime_check",
+        .root_module = lib_check,
+    });
+    check.dependOn(&check_compile.step);
 }
