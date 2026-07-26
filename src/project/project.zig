b038aecff24c98a296b49a62b3861429dea86b60
Jack Parsons <jackpar1113@gmail.com>
2026-07-26 17:09:20 -0600

    Asset Watcher (#22)
    * impl
    
    * bump zimp

--- src/project/project.zig
+++ src/project/project.zig
@@ -8,6 +8,21 @@ pub const Project = struct {
     manifest: zimp.ProjectManifest,
     root_dir: std.Io.Dir,
 
+    pub fn watchAssets(self: *const Project, allocator: std.mem.Allocator, io: std.Io) !*zimp.WatchHandle {
+        return zimp.WatchHandle.start(
+            allocator,
+            io,
+            self.manifest,
+            self.root_dir,
+            .{},
+            .{},
+        );
+    }
+
+    pub fn stopWatchingAssets(_: *Project, handle: *zimp.WatchHandle) void {
+        handle.stop();
+    }
+
     pub fn deinit(self: *Project, allocator: std.mem.Allocator, io: std.Io) void {
         self.manifest.deinit(allocator);
         self.root_dir.close(io);
