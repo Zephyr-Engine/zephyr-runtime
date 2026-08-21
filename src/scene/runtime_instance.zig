5abc055757a11f006471c57d38e99f6a79cbcc69
Jack Parsons <jackpar1113@gmail.com>
2026-08-21 07:44:06 -0600

    feedback

--- src/scene/runtime_instance.zig
+++ src/scene/runtime_instance.zig
@@ -125,6 +125,8 @@ pub fn addComponent(
 
 pub fn despawnEntity(self: *SceneRuntimeInstance, world: *World, id: zimp.SceneEntityId) !void {
     const entity = try self.resolve(id);
+    self.runtime_to_scene.remove(entity);
+    self.scene_to_runtime.remove(id);
     world.despawn(entity);
 }
 
